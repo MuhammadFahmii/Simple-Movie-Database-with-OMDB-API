28e19489ac58e5c0ef724927f8a9776c0572fe3d
Heinz <fahmiprasetio754@gmail.com>
2020-12-15 21:47:56 +0700

    Update movie.js

--- routes/omdb/movie.js
+++ routes/omdb/movie.js
@@ -1,12 +1,12 @@
 const app = require("express");
 const router = app.Router();
 const axios = require("axios");
-
+const API_KEY = 'YOUR_APIKEY";
 router.get("/", async (req, res) => {
   try {
     if (req.query.s) {
       await axios.default
-        .get(`http://www.omdbapi.com/?apikey=c8ecce7c&s=${req.query.s}`)
+        .get(`http://www.omdbapi.com/?apikey=${API_KEY}=${req.query.s}`)
         .then((result) => res.json(result.data.Search));
     } else if (req.query.i) {
       await axios.default
